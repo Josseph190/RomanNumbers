9f31fab61d9f89bdbb6f65e589d5a616113923f1
Jonathan <duncanjonathan@yahoo.ca>
2022-12-01 15:16:34 -0600

    Refactor

--- RomanNumbers/Converter.cs
+++ RomanNumbers/Converter.cs
@@ -2,21 +2,24 @@
 {
     public class Converter
     {
-        public static string Convert(int input)
+        private static (char, int)[] rCV = { ('M', 1000), ('D', 500), ('C', 100), ('L', 50), ('X', 10), ('V', 5), ('I', 1) };
+        public static string Convert(int input, (char, int)[]? rcV1 = null)
         {
-            var result1000 = ToRoman(1000, 'M', input);
-            var result500 = ToRoman(500, 'D', input);
-            var result100 = ToRoman(100, 'C', input);
-            var result50 = ToRoman(50, 'L', input);
-            var result10 = ToRoman(10, 'X', input);
-            var result5 = ToRoman(5, 'V', input);
+            rcV1 = rcV1 ?? rCV;
+
+            var result1000 = ToRoman('M', 1000, input);
+            var result500 = ToRoman('D', 500, input);
+            var result100 = ToRoman('C', 100, input);
+            var result50 = ToRoman('L', 50, input);
+            var result10 = ToRoman('X', 10, input);
+            var result5 = ToRoman('V', 5, input);
             var result4 = (input > 3) ? "IV" : null;
-            var result1 = ToRoman(1, 'I', input);
+            var result1 = ToRoman('I', 1, input);
 
             return result1000 ?? result500 ?? result100 ?? result50 ?? result10 ?? result5 ?? result4 ?? result1 ?? "";
         }
 
-        private static string? ToRoman(int baseNum, char romanLetter, int input)
+        private static string? ToRoman(char romanLetter, int baseNum, int input)
         {
             if (input < baseNum)
             {
@@ -24,9 +27,7 @@
             }
             else
             {
-                var quotient = input / baseNum;
-                var remainder = input % baseNum;
-                return Repetir(romanLetter, quotient) + Convert(remainder);
+                return Repetir(romanLetter, input / baseNum) + Convert(input % baseNum);
             }
         }
 
